d1d7c760ca9b59b0b9db35eb0b78e8d478558268
Prathamesh Kulkarni <prathameshk2696@gmail.com>
2021-08-21 00:31:14 -0400

    Added test functions for nonparametric and parametric bootstrap confidence intervals. Refactored the test functions.

--- resample/bootstrap/tests/test_base.py
+++ resample/bootstrap/tests/test_base.py
@@ -7,76 +7,89 @@ from numpy.testing import assert_allclose
 
 from resample.bootstrap import NonparametricBootstrap, ParametricBootstrap
 
-# TODO: add test for bootstrap confidence interval
+# TODO: add test for bootstrap confidence intervals - BCa and ABC.
 
 atol = 1e-10
 random_seed = 42
 
-def test_nonparametric_bootstrap_bias():
-    sample1 = np.array([94, 197, 16, 38, 99, 141, 23], dtype = float)
-    sample2 = np.array([52, 104, 146, 10, 51, 30, 40, 27, 46], dtype = float)
-    def estimate_func(sample1, sample2):
-        return sample1.mean() - sample2.mean()
-    npboot = NonparametricBootstrap(sample1, sample2, estimate_func = estimate_func, B0 = 500, seed = random_seed)
-    npboot.fit()
-    actual_bias = npboot.bias()
-    desired_bias = -1.0437142857142945
-    assert_allclose(actual = actual_bias, desired = desired_bias, atol = atol)
+class TestNonparametricBootstrap:
 
-def test_nonparametric_bootstrap_var():
-    sample1 = np.array([94, 197, 16, 38, 99, 141, 23], dtype=float)
-    sample2 = np.array([52, 104, 146, 10, 51, 30, 40, 27, 46], dtype=float)
-    def estimate_func(sample1, sample2):
-        return sample1.mean() - sample2.mean()
-    npboot = NonparametricBootstrap(sample1, sample2, estimate_func = estimate_func, B0 = 500, seed = random_seed)
-    npboot.fit()
-    actual_variance = npboot.var()
-    desired_variance = 663.6485178681878
-    assert_allclose(actual = actual_variance, desired = desired_variance, atol = atol)
+    @pytest.fixture(scope = 'class')
+    def npboot(self):
+        sample = np.array([52, 104, 146, 10, 51, 30, 40, 27, 46], dtype = float)
+        def estimate_func(sample):
+            return sample.mean()
+        npboot = NonparametricBootstrap(sample, estimate_func = estimate_func,B0 = 1000, B1 = 25, cl = 95, seed = random_seed)
+        npboot.fit()
+        return npboot
 
-def test_nonparametric_bootstrap_std():
-    sample1 = np.array([94, 197, 16, 38, 99, 141, 23], dtype=float)
-    sample2 = np.array([52, 104, 146, 10, 51, 30, 40, 27, 46], dtype=float)
-    def estimate_func(sample1, sample2):
-        return sample1.mean() - sample2.mean()
-    npboot = NonparametricBootstrap(sample1, sample2, estimate_func = estimate_func, B0 = 500, seed = random_seed)
-    npboot.fit()
-    actual_std = npboot.std()
-    desired_std = 25.761376474641022
-    assert_allclose(actual = actual_std, desired = desired_std, atol = atol)
+    def test_bias(self, npboot):
+        actual_bias = npboot.bias()
+        desired_bias = -0.6008888888888961
+        assert_allclose(actual = actual_bias, desired = desired_bias, atol = atol)
 
-def test_parametric_bootstrap_bias():
-    sample1 = np.array([94, 197, 16, 38, 99, 141, 23], dtype=float)
-    sample2 = np.array([52, 104, 146, 10, 51, 30, 40, 27, 46], dtype=float)
-    def estimate_func(sample1, sample2):
-        return sample1.mean() - sample2.mean()
-    pboot = ParametricBootstrap(sample1, sample2, estimate_func=estimate_func, B0 = 200, dists = ('norm', 'norm'), seed = random_seed)
-    pboot.fit()
-    actual_bias = pboot.bias()
-    desired_bias = 0.3561565729846521
-    assert_allclose(actual = actual_bias, desired = desired_bias, atol = atol)
+    def test_var(self, npboot):
+        actual_variance = npboot.var()
+        desired_variance = 173.91534171208247
+        assert_allclose(actual = actual_variance, desired = desired_variance, atol = atol)
 
-def test_parametric_bootstrap_var():
-    sample1 = np.array([94, 197, 16, 38, 99, 141, 23], dtype=float)
-    sample2 = np.array([52, 104, 146, 10, 51, 30, 40, 27, 46], dtype=float)
-    def estimate_func(sample1, sample2):
-        return sample1.mean() - sample2.mean()
-    pboot = ParametricBootstrap(sample1, sample2, estimate_func=estimate_func, B0 = 200, dists=('norm', 'norm'), seed = random_seed)
-    pboot.fit()
-    actual_variance = pboot.var()
-    desired_variance = 718.0531559250003
-    assert_allclose(actual = actual_variance, desired = desired_variance, atol = atol)
+    def test_std(self, npboot):
+        actual_std = npboot.std()
+        desired_std = 13.187696603731922
+        assert_allclose(actual = actual_std, desired = desired_std, atol = atol)
 
-def test_parametric_bootstrap_std():
-    sample1 = np.array([94, 197, 16, 38, 99, 141, 23], dtype=float)
-    sample2 = np.array([52, 104, 146, 10, 51, 30, 40, 27, 46], dtype=float)
-    def estimate_func(sample1, sample2):
-        return sample1.mean() - sample2.mean()
-    pboot = ParametricBootstrap(sample1, sample2, estimate_func=estimate_func, B0 = 200, dists=('norm', 'norm'), seed = random_seed)
-    pboot.fit()
-    actual_std = pboot.std()
-    desired_std = 26.796513876342203
-    assert_allclose(actual = actual_std, desired = desired_std, atol = atol)
+    def test_ci_basic(self, npboot):
+        actual_ci = npboot.ci(method = 'basic')
+        desired_ci = (28.77777777777777, 80.44444444444444)
+        assert_allclose(actual = actual_ci, desired = desired_ci)
 
+    def test_ci_studentized(self, npboot):
+        actual_ci = npboot.ci(method = 'studentized')
+        desired_ci = (31.692849205061204, 128.4648433907318)
+        assert_allclose(actual = actual_ci, desired = desired_ci)
 
+    def test_ci_percentile(self, npboot):
+        actual_ci = npboot.ci(method = 'percentile')
+        desired_ci = (32.0, 83.66666666666667)
+        assert_allclose(actual = actual_ci, desired = desired_ci)
 
+class TestParametricBootstrap:
+
+    @pytest.fixture(scope='class')
+    def pboot(self):
+        sample = np.array([52, 104, 146, 10, 51, 30, 40, 27, 46], dtype = float)
+        def estimate_func(sample):
+            return sample.mean()
+        pboot = ParametricBootstrap(sample, estimate_func = estimate_func, B0 = 1000, B1 = 25, cl = 95, dists = ('norm',), seed = 42)
+        pboot.fit()
+        return pboot
+
+    def test_bias(self, pboot):
+        actual_bias = pboot.bias()
+        desired_bias = 0.0912969794016405
+        assert_allclose(actual = actual_bias, desired = desired_bias, atol = atol)
+
+    def test_var(self, pboot):
+        actual_variance = pboot.var()
+        desired_variance = 181.46581217398094
+        assert_allclose(actual = actual_variance, desired = desired_variance, atol = atol)
+
+    def test_std(self, pboot):
+        actual_std = pboot.std()
+        desired_std = 13.47092469632211
+        assert_allclose(actual = actual_std, desired = desired_std, atol = atol)
+
+    def test_ci_basic(self, pboot):
+        actual_ci = pboot.ci(method = 'basic')
+        desired_ci = (28.905200841525172, 82.40808725987921)
+        assert_allclose(actual = actual_ci, desired = desired_ci)
+
+    def test_ci_studentized(self, pboot):
+        actual_ci = pboot.ci(method = 'studentized')
+        desired_ci = (19.277766301169898, 90.81767743253205)
+        assert_allclose(actual = actual_ci, desired = desired_ci)
+
+    def test_ci_percentile(self, pboot):
+        actual_ci = pboot.ci(method = 'percentile')
+        desired_ci = (30.036357184565233, 83.53924360291927)
+        assert_allclose(actual = actual_ci, desired = desired_ci)
